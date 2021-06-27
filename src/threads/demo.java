eb028bd2c85600d882291797d0bfab7dfa8c42e8
rahulsain <50570677+rahulsain@users.noreply.github.com>
2021-06-27 13:59:55 +0530

    added demo temp class

---
+++ src/threads/demo.java
@@ -0,0 +1,12 @@
+package threads;
+
+public class demo {
+    void demo() /* Line 3 */
+    {
+        System.out.println("Class DEMO");
+    }
+    public static void main(String[] args)
+    {
+        new demo();
+    }
+}
