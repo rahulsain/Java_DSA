cf246f5293f29bf318b5ce1a0a0a6d47ec7510cd
rahulsain <50570677+rahulsain@users.noreply.github.com>
2021-06-27 15:34:33 +0530

    minor update

--- src/threads/demo.java
+++ src/threads/demo.java
@@ -1,12 +1,14 @@
 package threads;
 
-public class demo {
-    void demo() /* Line 3 */
-    {
-        System.out.println("Class DEMO");
-    }
+class demo
+{
     public static void main(String[] args)
     {
-        new demo();
+        int value = 3, sum = 6 + -- value;
+        // value = 2, sum = 8
+        int data = --value + ((++value / sum++) * value++) + (++sum % value--);
+        //              2           3       9       1            9      2
+        System.out.println(data+ " " + sum+ " "+ value); // 2 10 2
     }
 }
+
