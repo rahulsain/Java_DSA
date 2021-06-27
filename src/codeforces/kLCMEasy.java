e36ea26d300be33429543ef77fb0c17aab5450a7
rahulsain <50570677+rahulsain@users.noreply.github.com>
2021-06-27 10:31:40 +0530

    added codeforce soln

---
+++ src/codeforces/kLCMEasy.java
@@ -0,0 +1,31 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class kLCMEasy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            if (n % 2 == 0 && (n / 2) % 2 == 0) {
+                System.out.println(n / 2 + " " + n / 4 + " " + n / 4);
+            } else if (n % 2 == 0 && (n / 2) % 2 == 1) {
+                int c = n - 2;
+                System.out.println(c / 2 + " " + c / 2 + " " + (n - c));
+            } else {
+                System.out.println(n / 2 + " " + n / 2 + " " + (n - (n / 2 + n / 2)));
+            }
+        }
+    }
+    // static int lcm(int x, int y){
+    //     return ((x*y)/gcd(x,y));
+    // }
+    // static int gcd(int a, int b){
+    //     if (b == 0)
+    //         return a;
+    //     return gcd(b, a % b);
+    // }
+}
---
+++ src/greedy/bigArrayscf.java
@@ -0,0 +1,41 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class bigArrayscf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                arr.add(sc.nextInt());
+            }
+            System.out.println(findAvgDel(arr, n));
+        }
+
+        sc.close();
+    }
+
+    private static int findAvgDel(ArrayList<Integer> a, int n) {
+        int ans = 0;
+        Collections.sort(a);
+        int temp = n;
+        while (temp-- > 0) {
+            int avg = (a.get(0) + a.get(a.size() - 1)) / 2;
+
+            for (int i = 0; i < a.size(); i++) {
+                if (a.get(i) > avg) {
+                    a.remove(i);
+                    ans++;
+                }
+
+            }
+        }
+
+        return ans;
+    }
+}
